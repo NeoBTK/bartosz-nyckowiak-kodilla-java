16958f9c1c573699cd293998dc100af3919a4d67
NeoBTK <neobtk@gmail.com>
2025-02-04 17:53:39 +0100

    7.3 Fix. Adding class 'StreamMain' with solution, to 'com.kodilla.stream'

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,17 +1,34 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
-    public static void main(String[] args) {
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        System.out.println(poemBeautifier.beautify("Abracadabra", s -> "ABC" + s + "ABC"));
-        System.out.println(poemBeautifier.beautify("Abracadabra", s -> s.toUpperCase()));
-        System.out.println(poemBeautifier.beautify("Abracadabra", s -> s.replace("a", "o")));
-        System.out.println(poemBeautifier.beautify("Abracadabra", s -> s + s.length()));
-        System.out.println(poemBeautifier.beautify("Abracadabra", s -> {String s1 = s.substring(4);
-                                                                                        String s2 = s.substring(0, 4);
-                                                                                        return s1 + s2;}));
+    public static void main(String[] args) {
+        Forum forumUsers = new Forum();
+
+        Map<Integer, ForumUser> forumUsersResultMap = forumUsers.getUserList().stream()
+                .filter(user -> user.getGender() == 'M')
+                .filter(user -> calculateAge(user.getBirthday()) >= 20)
+                .filter(user -> user.getPublishedPosts() > 0)
+                .collect(Collectors.toMap(ForumUser::getId, user -> user));
+
+        System.out.println("Result:");
+        forumUsersResultMap.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+//        forumUsersResultMap.forEach((k, v) -> System.out.println(k + ": " + v));
+    }
+
+    public static int calculateAge(LocalDate birthDate) {
+        LocalDate today = LocalDate.now();
+        Period age = Period.between(birthDate, today);
+        return age.getYears();
     }
 }
