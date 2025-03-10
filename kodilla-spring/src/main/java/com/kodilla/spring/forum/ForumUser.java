b248cdbc48eaeec1fe68ee716c59c8ef38ade73a
NeoBTK <neobtk@gmail.com>
2025-03-10 02:04:29 +0100

    First commit. Adding module 'kodilla-spring' with package 'com.kodilla.spring.forum'

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,14 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+    private final String username;
+
+    public ForumUser() {
+        this.username = "John Smith";
+    }
+
+    public String getUsername() { return username; }
+}
